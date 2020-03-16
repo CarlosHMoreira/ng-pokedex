5ed2caf3cd0a5bef6298dd13dc47f029c7c5d27f
Carlos Henrique da S. Moreira <carlos.moreira.tech@gmail.com>
2020-03-15 22:52:25 -0300

    style list component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout/layout.component';
 import { ListComponent } from './list/list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { ListComponent } from './list/list.component';
     ListComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   exports: [
     LayoutComponent,
