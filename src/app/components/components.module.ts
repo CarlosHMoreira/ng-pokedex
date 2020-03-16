194b6b230243bee12fbb6bbade9df770cb0055d4
Carlos Henrique da S. Moreira <carlos.moreira.tech@gmail.com>
2020-03-15 21:24:27 -0300

    creates list and list-item component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout/layout.component';
+import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list/list-item/list-item.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, LayoutComponent],
+  declarations: [HeaderComponent, LayoutComponent, ListComponent, ListItemComponent],
   imports: [
     CommonModule
   ],
---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NameUrlDTO } from '../../models/poke-api.model';
+
+@Component({
+  selector: 'pkx-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+  @Input() list = new Observable<NameUrlDTO>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
